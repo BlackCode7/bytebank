87136edcd14a37d240dacb437f15b2d41b4b01c1
Anderson <BlackCode7@outlook.com>
2022-01-03 21:18:32 -0300

    Aprofundando testes com karma e jasmine

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,7 +4,7 @@ import { ExtratoComponent } from "./extrato/extrato.component";
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component'
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'extrato', pathMatch: 'full'},
+    {path: '/', redirectTo: 'extrato', pathMatch: 'full'},
     {path: 'extrato', component:ExtratoComponent},
     {path: 'nova-transferencia', component:NovaTransferenciaComponent}
 ]
--- src/app/services/transferencia.service.spec.ts
+++ src/app/services/transferencia.service.spec.ts
@@ -1,16 +1,109 @@
-import { TestBed } from '@angular/core/testing';
-
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TransferenciaService } from './transferencia.service';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ExtratoComponent } from '../extrato/extrato.component';
+import { NovaTransferenciaComponent } from '../nova-transferencia/nova-transferencia.component';
+import { AppComponent } from '../app.component';
+import { APP_BASE_HREF } from '@angular/common';
+//import { of } from 'rxjs';
 
-describe('TransferenciaService', () => {
-  let service: TransferenciaService;
+fdescribe('TransferenciaService', () => { // O 'f' no inicio avisa ao jasmine que so vou rodar este teste
+  let service: TransferenciaService; // Declara o serviço
+  let http: HttpClient;
+
+  const url = 'http://localhost:3000/tbByteBank' 
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TransferenciaService);
+    TestBed.configureTestingModule({
+     imports: [
+      HttpClientTestingModule
+    ]
+    }); // Aqui configura um modulo de teste
+    service = TestBed.inject(TransferenciaService); // Aqui ele faz o injeção do teste
+    http = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
+  it('should be created', () => { // Teste Padrão criado pelo angular CLI
     expect(service).toBeTruthy();
   });
+   
+  it('Método todas deve fazer um get', async () => { // Escrevendo teste do método todas() // Colocando um espião    
+    const spy = spyOn(http, 'get').and.callThrough(); //Chamamos este método .and.callThrough() para evitar travamentos no teste
+    service.todas(); //Chamo o metodo do service aqui todas()
+    expect(spy).toHaveBeenCalledWith(url); //Aqui eu espero a chamada do spy
+  }); 
+
 });
+
+describe('AppComponent', () => {
+  const routes: Routes = [
+    {path: '', redirectTo: 'extrato', pathMatch: 'full'},
+    {path: 'extrato', component:ExtratoComponent},
+    {path: 'nova-transferencia', component:NovaTransferenciaComponent}
+  ];
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        ExtratoComponent,
+        NovaTransferenciaComponent        
+      ],
+      imports: [
+        RouterModule.forRoot(routes),
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue:'/'}
+      ]
+    }).compileComponents();
+  }))
+
+it(`Testando tag <a> de <header> bytebank `, async(() => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.debugElement.componentInstance;
+  expect(app.title).toEqual('Nova Transferenci');
+}))
+
+it(`Testando tag <a> de <header> bytebank `, async(() => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.debugElement.componentInstance;
+  expect(app.title).toEqual('ByteBank');
+}))
+
+it(`Testando se o AppComponent tem o título bytebank `, async(() => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const app = fixture.debugElement.nativeElement;
+  expect(app.querySelector('a').textContent).toContain('bytebank');
+}))
+
+
+})
+
+
+
+
+
+// fdescribe('TransferenciaService', () => { // O 'f' no inicio avisa ao jasmine que so vou rodar este teste
+//   let service: TransferenciaService; // Declara o serviço
+//   let http: HttpClient;
+
+//   const url = 'http://localhost:3000/tbByteBank' 
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//      imports: [
+//       HttpClientTestingModule
+//     ]
+//     }); // Aqui configura um modulo de teste
+//     service = TestBed.inject(TransferenciaService); // Aqui ele faz o injeção do teste
+//     http = TestBed.inject(HttpClient);
+//   });
+
+//   it('should be created', () => { // Teste Padrão criado pelo angular CLI
+//     expect(service).toBeTruthy();
+//   });
+// });
+
+// OBS >>> Para rodar um teste com jasmine devemos usar o >>> npm test
